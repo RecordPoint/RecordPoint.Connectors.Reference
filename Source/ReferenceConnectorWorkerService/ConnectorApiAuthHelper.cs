6ec316324a5065979d2caa59e312a60eb0290d48
JBettonvil <46331458+JBettonvil@users.noreply.github.com>
2019-01-24 13:30:52 +1100

    Update ConnectorApiAuthHelper.cs

--- Source/ReferenceConnectorWorkerService/ConnectorApiAuthHelper.cs
+++ Source/ReferenceConnectorWorkerService/ConnectorApiAuthHelper.cs
@@ -9,14 +9,14 @@ namespace ReferenceConnectorWorkerService
         {
             return new ApiClientFactorySettings
             {
-                ConnectorApiUrl = "https://connector-uat.eigerio.space/connector/" // TODO: add endpoint address here
+                ConnectorApiUrl = "" // TODO: add endpoint address here
             };
         }
 
         public static AuthenticationHelperSettings GetAuthenticationHelperSettings(string tenantDomainName)
         {
             var secureString = new SecureString();
-            var clearSecret = "bvriZYbkSDFgK8hOYokuynWyK8l8vA+iCV+JwRmd4nE=";   // TODO: add client secret here
+            var clearSecret = "";   // TODO: add client secret here
             foreach (var c in clearSecret)
             {
                 secureString.AppendChar(c);
@@ -24,8 +24,8 @@ namespace ReferenceConnectorWorkerService
 
             return new AuthenticationHelperSettings
             {
-                AuthenticationResource = "https://olympusmons.onmicrosoft.com/ReferenceConnector", // TODO: add authentication resource here
-                ClientId = "59c8ca1e-daea-4c15-816a-6a07ed4e27fc", // TODO: add clientid here
+                AuthenticationResource = "", // TODO: add authentication resource here
+                ClientId = "", // TODO: add clientid here
                 ClientSecret = secureString,
                 TenantDomainName = tenantDomainName
             };
